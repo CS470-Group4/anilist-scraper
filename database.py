729cec362b509748d9f84687c4a1a21bf0432768
Alice <mail-alice@riseup.net>
2022-05-01 17:58:35 -0500

    Insert things into the database

--- database.py
+++ database.py
@@ -14,5 +14,19 @@ connection = pymysql.connect(host='localhost', user=db_username, password=db_pw,
 
 anime_obj = download.get_anime()
 
-#for item in anime_obj:
-#  print(item['title']['english'], item['coverImage']['medium'], item['studios']['nodes'][0]['name'], item['relations']['nodes'])
+for item in anime_obj:
+  print(item['title']['english'], item['coverImage']['medium'], item['studios']['nodes'][0]['name'])
+  try:  
+    print( "MANGA: ", item['relations']['nodes'][0]['title']['english'])
+  except IndexError:
+    print("NO MANGA")
+
+  with connection.cursor() as cursor:
+    sql = "SELECT `NAME` FROM `ANIME_SHOW` WHERE `NAME` = %s"
+    cursor.execute(sql, (item['title']['english'],))
+    result = cursor.fetchone()
+    print(result)
+    if (result == None):
+      insertsql = "INSERT INTO `ANIME_SHOW` (`NAME`, `DESCRIPTION`, `PIC`, `STUDIO`) VALUES (%s, %s, %s, %s)"
+      cursor.execute(insertsql, (item['title']['english'], item['description'], item['coverImage']['medium'], item['studios']['nodes'][0]['name']))
+    connection.commit()
